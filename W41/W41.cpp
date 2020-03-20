773d90d47e5926908ece8378499ddfe5ed3c53da
严浩 <1424412158@qq.com>
2020-03-20 14:58:11 +0800

    1

--- W41/W41.cpp
+++ W41/W41.cpp
@@ -1,11 +1,31 @@
 ﻿// W41.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
-
 #include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat binaryMat;
+	cv::Mat ErodeMat;
+	cv::Mat DilateMat;
+	cv::Mat OpenMat;
+	cv::Mat CloseMat;
+	cv::Mat srcMat = imread("c://project//coin.png", 0);
+	cv::Mat kernel;
+	kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
+	cv::morphologyEx(binaryMat, ErodeMat, 0, kernel);
+	cv::morphologyEx(binaryMat, DilateMat, 1, kernel);
+	cv::morphologyEx(binaryMat, OpenMat, 2, kernel);
+	cv::morphologyEx(binaryMat, CloseMat, 3, kernel);
+	imshow("ErodeMat", ErodeMat);
+	imshow("DilateMat", DilateMat);
+	imshow("OpenMat", OpenMat);
+	imshow("CloseMat", CloseMat);
+	waitKey(0);
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
